988fc0880ac492ca111873985adbabda0f14a344
at-longdau <longdau@asiantech.vn>
2017-12-15 10:01:37 +0700

    update task controller

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -10,49 +10,34 @@ use App\Repositories\TaskRepository;
 
 class TaskController extends Controller
 {
-    /**
-     * The task repository instance.
-     *
-     * @var TaskRepository
-     */
-    protected $tasks;
-
-    /**
-     * Create a new controller instance.
-     *
-     * @param  TaskRepository  $tasks
-     * @return void
-     */
-    public function __construct(TaskRepository $tasks)
+        protected $tasks;
+    //
+        public function __construct(TaskRepository $tasks)
     {
         $this->middleware('auth');
 
         $this->tasks = $tasks;
     }
 
-    /**
-     * Display a list of all of the user's task.
-     *
-     * @param  Request  $request
-     * @return Response
-     */
-    public function index(Request $request)
+        public function index(Request $request)
     {
         return view('tasks.index', [
             'tasks' => $this->tasks->forUser($request->user()),
         ]);
     }
-    public function store(Request $request)
-{
-    $this->validate($request, [
-        'name' => 'required|max:255',
-    ]);
 
-    $request->user()->tasks()->create([
-        'name' => $request->name,
-    ]);
-
-    return redirect('/tasks');
-}
-
+        public function store(Request $request){
+            $this->validate($request,[
+                'name' => 'required | max:255',
+            ]);
+            $request->user()->tasks()->create([
+                'name' => $request->name,
+            ]);
+            return redirect('/tasks');
+        }
+        public function destroy(Request $request, Task $task){
+            $this->authorize('destroy', $task);
+            $task->delete();
+            return redirect('tasks');
+        }
 }
